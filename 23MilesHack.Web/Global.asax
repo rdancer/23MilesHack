cf383de7050cf372f5ed55f25b70a547868ce13b
Anish <anish.patel@thap.co.uk>
2013-07-06 13:03:01 +0100

    initial commit

---
+++ 23MilesHack.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23MilesHack.Web.MvcApplication" Language="C#" %>
---
+++ 23MilesHack.Web/js/main.js
@@ -0,0 +1,20 @@
+
+(function (window, $, undefined) {
+
+
+    window.URL = window.URL || window.webkitURL;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    navigator.getUserMedia({ video: true, audio: true }, function(stream) {
+        var video = $("#my-webcam")[0];
+
+        video.src = window.URL.createObjectURL(stream);
+    }, function(e) {
+        console.log(e);
+    });
+
+
+})(window, $);
+
+
